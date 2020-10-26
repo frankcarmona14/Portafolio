28ff32287ffe027ae6ccab8d7ce4ca893e7740af
Frank Carmona <frankcarmonavasquez@gmail.com>
2020-10-26 16:01:20 +0100

    empezando a maquetar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <h1>Hello</h1>
-  );
+  return <Header />;
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => (
+<header>
+    <a href="/">
+        <span className="logo-tag">&lt;</span>
+        <span className="name">Frank</span>
+        <span className="logo-tag">/&gt;</span>
+    </a>
+    <nav>
+        <ul>
+            <li>Sobre mi</li>
+            <li>Portafolio</li>
+            <li></li>
+        </ul>
+    </nav>
+</header>
+);
+
+export default Header;
