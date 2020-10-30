f6b2a9645cbb213db92c4516ca4f089924df5d37
Frank Carmona <frankcarmonavasquez@gmail.com>
2020-10-30 04:57:54 +0100

    arreglado

---
+++ build/precache-manifest.93ec5d33536d879898d39872980928db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0834032070c87b218fadbaee38ba16d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ee0052dbeb8e44a312f",
+    "url": "/static/css/main.a9293691.chunk.css"
+  },
+  {
+    "revision": "cc3c365e008e20946186",
+    "url": "/static/js/2.c66afac5.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/static/js/2.c66afac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee0052dbeb8e44a312f",
+    "url": "/static/js/main.d15f301f.chunk.js"
+  },
+  {
+    "revision": "4569d4d938faf031954c",
+    "url": "/static/js/runtime-main.6109b15a.js"
+  }
+]);
