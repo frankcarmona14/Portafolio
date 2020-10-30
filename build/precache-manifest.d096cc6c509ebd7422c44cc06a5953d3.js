33609bb7b94d8cb12594fc636a275e0643a697d3
Frank Carmona <frankcarmonavasquez@gmail.com>
2020-10-30 05:30:59 +0100

    actualizadooo

---
+++ build/precache-manifest.d096cc6c509ebd7422c44cc06a5953d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ae947e6eff611aa2b24e5794ad9d42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9906b7e7f4936fe6a63f",
+    "url": "/static/css/main.a9293691.chunk.css"
+  },
+  {
+    "revision": "cc3c365e008e20946186",
+    "url": "/static/js/2.c66afac5.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/static/js/2.c66afac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9906b7e7f4936fe6a63f",
+    "url": "/static/js/main.99c4bf91.chunk.js"
+  },
+  {
+    "revision": "4569d4d938faf031954c",
+    "url": "/static/js/runtime-main.6109b15a.js"
+  }
+]);
